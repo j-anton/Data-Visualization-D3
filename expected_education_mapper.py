0ab1f4af968139b4cd30c74a0a4c4485fcf5b903
BGoldman1989 <bradygoldman@gmail.com>
2017-03-15 18:23:34 +0100

    added script for creating map of expected level of education completed to occupation

---
+++ expected_education_mapper.py
@@ -0,0 +1,29 @@
+import pandas
+import numpy
+
+#Determines expected level of education attained for a particular occupation
+def expected_education(x,highest_educations_attained):
+    sum = 0
+    total = numpy.sum(x[1:])
+    for i in xrange(0,len(x)-1):
+        sum += i * x[highest_educations_attained[i]]
+    expected_value = sum/total
+    return highest_educations_attained[numpy.round(expected_value)]
+
+#----------------------------------------------------------------------------------------------------------------------#
+
+#Import csv
+education_data = pandas.read_csv("data/education_attained_by_occupation.csv")
+
+#Clean data
+highest_educations_attained = education_data.columns.values[-5:]
+education_data = education_data[numpy.hstack(([' '],highest_educations_attained))][1:481]
+
+#Calculate expected highest level of education completed for each occupation
+education_data['Expected Highest Education Level Completed'] = \
+    education_data.apply(lambda x: expected_education(x,highest_educations_attained), axis = 1)
+
+#Output to csv
+education_data = education_data[[' ','Expected Highest Education Level Completed']]
+education_data.columns = ['Occupation','Expected Highest Education Level Completed']
+education_data.to_csv("data/expected_education_attained_by_occupation.csv")
